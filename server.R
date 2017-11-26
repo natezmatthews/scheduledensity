192722c677549376af2a9719738feaf6799412bc
Nate Matthews <natezmatthews@gmail.com>
2017-11-25 23:38:55 -0500

    Moved weekday logic to the app

--- server.R
+++ server.R
@@ -41,22 +41,35 @@ prep.for.plot <- function(df,fromdt,todt) {
   return(toplot)
 }
 
-weekday.plot <- function(df,fromdt,todt,eras=1) {
+weekday.plot <- function(df,fromdt,todt) {
   # Restrict to the daterange from the slider
   df <- df[fromdt <= df$startdt & todt > df$enddt,]
   
   half.hour.of.day <- function(x) {(x$hour*60 + x$min) %/% 30}
-  events.matrix <- matrix(nrow=24*2,ncol=7)
-  count.matrix <- matrix(nrow=24*2,ncol=7)
+  events.matrix <- matrix(list(),nrow=24*2,ncol=7)
+  count.matrix <- matrix(0,nrow=24*2,ncol=7)
   for(j in 1:7) {
     for(i in 1:(24*2)) {
-      events.matrix[i,j] <- df$summary[(half.hour.of.day(df$start) <= i) & (half.hour.of.day(df$end) > i) & (df$start$wday == j)]
-      count.matrix[i,j] <- length(events.matrix[i,j])
+      events.matrix[i,j] <- list(df$summary[(half.hour.of.day(df$start) <= i)
+                                            & (half.hour.of.day(df$end) > i)
+                                            & (df$start$wday == j-1)])
+      count.matrix[i,j] <- lengths(events.matrix[i,j])
     }
   }
-  half.hours.possible <- as.integer(todt - min(df$startdt))/7
+  half.hours.possible <- as.integer(Sys.Date() - min(df$startdt))/7
   
-  
+  toplot <- as.data.frame(count.matrix / half.hours.possible)
+  toplot <- cbind(toplot,seq(
+    from=as.POSIXct("2017-1-1 0:00"),
+    to=as.POSIXct("2017-1-1 23:30"),
+    by=60*30
+  )
+  )
+  colnames(toplot) <- c("Mon","Tue","Wed","Thu","Fri","Sat","Sun","Time")
+  for (i in 1:7) {
+    toplot[,i] <- as.numeric(as.character(toplot[,i]))
+  }
+  return(toplot)
 }
 
 # Define server logic required to draw a histogram
@@ -79,7 +92,15 @@ function(input, output) {
   
   output$distPlot <- renderPlot({
     # Plot!
-    toplot <- prep.for.plot(mydf,input$date_range[1],input$date_range[2])
-    ggplot(data=toplot, aes(x=halfhr,y=cnt)) + geom_line()
+    toplot <- weekday.plot(mydf,input$date_range[1],input$date_range[2])
+    colors <- c("#E69F00", "#56B4E9", "#009E73", "#F0E442", "#0072B2", "#D55E00", "#CC79A7")
+    p <- ggplot(toplot,aes(x=Time)) + 
+      scale_x_datetime("", date_labels = "%I:%M %p") +
+      scale_y_continuous(labels=percent) +
+      ylab("% of weeks with plans")
+    for (i in 1:7) {
+      p <- p + geom_line(aes_string(y = colnames(toplot)[i]),colour=colors[i])
+    }
+    return(p)
   })
 }
